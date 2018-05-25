fe67823d087861a4dc4f08bc6bd9477e4b37b43e
Dameon1 <dameonmedoza@gmail.com>
2018-05-25 15:04:42 -0700

    delete enpoint

--- src/components/cheese-list.js
+++ src/components/cheese-list.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { fetchCheeses } from '../actions/cheese';
-
+import { fetchCheeses , fetchRecipes, postCheese,removeCheese } from '../actions/cheese';
+import "./cheese.css";
 export class  CheeseList extends React.Component {
   
 //this is needed to populate the cheeses array
 componentDidMount() {
   this.props.dispatch(fetchCheeses());
 }
+
+    
+    handleSubmit(e){
+      e.preventDefault();
+      const value = this.input.value;
+      this.props.dispatch(postCheese({title:value}));
+      this.input.value = '';
+      this.input.focus();
+      this.props.dispatch(fetchCheeses());
+  }
     
     render(){
         return (
           <ul>
+            
             <button onClick={()=>this.props.dispatch(fetchCheeses())}>Hello</button>
-            <button onClick={()=>console.log("Hello")}>Hello</button>
-              {this.props.cheeses.map((cheese,index) => (
-               <li key={index}>
-                 {cheese}
+            <button onClick={()=>this.props.dispatch(fetchRecipes())}>Hello</button>
+            <h2>Add a Cheese</h2>
+            <form id="cheeseForm" onSubmit={e=>this.handleSubmit(e)}>
+            <input id='cheese' type="text"  ref={input => (this.input = input)}/>
+            <button type="submit" onClick={(e)=>{this.handleSubmit(e);}} >Add cheese</button>
+            </form>
+              { this.props.cheeses.map((cheese,index) => (
+               <li key={index} ref={index => (this.index = index)}>
+                  {cheese.title}{console.log(cheese.title)}
+                  <button onClick={()=>{this.props.dispatch(removeCheese(cheese.id))}}   className="delete" id="delete" >x</button>
                </li>
             ))}
           </ul>
@@ -27,51 +44,4 @@ const mapStateToProps = state => ({
     cheeses: state.cheeses || []
 })
 export default connect(mapStateToProps)(CheeseList) 
-
-
-
-
-
-  // fetchCheeses = () => {
-  // this.props.dispatch(fetchCheeses());
-// }
-//     let cheese = fetchCheeses();
-//     console.log(cheese);
-
-    
-// render(){
-//   let cheeses = props.cheeses.map((cheese,index) => {
-//       return <li key={index}>{cheese}</li>
-//     });
-// render(){
-//   let cheeses = ['gooda'];
-//     return (
-//       <ul>
-//         {cheeses}
-//         <button onClick={()=>this.props.dispatch(fetchCheeses())}>Hello</button>
-//       </ul>
-//     )
-//   }
-
-
-
- 
-
-// render(){
-  
-//     return (
-//       <ul>
-//         {this.props.cheeses.map(cheese => (
-//                     <li >{cheese}</li>
-//                 ))}
-        
-//         <button onClick={()=>fetchCheeses()}>Hello</button>
-//       </ul>
-//     )
-//   }
-// }
-
-
-
-
 
